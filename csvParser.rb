2e5df01f63363e6be9cb019833bfdbae061bd405
Daniel Montgomery <daniel.w.montgomery@gmail.com>
2019-08-21 09:39:33 -0700

    should be working well now

--- csvParser.rb
+++ csvParser.rb
@@ -5,6 +5,22 @@ puts "Parsing the file..."
 table = CSV.read(ARGV[0], headers: true)
 puts ARGV[0]
 
+# Clean up the table data:
+# Remove Parentheses in company names, then remove special characters.
+encoding_options = {
+  :invalid => :replace,
+  :undef => :replace,
+  :replace => " ",
+  :universal_newline => true #probably an unnecessary line for this script.
+}
+
+for i in table
+  i[1] = i[1].tr("()", "")
+  i[1] = i[1].encode(Encoding.find("ASCII"), encoding_options)
+  i[2] = i[2].tr("()", "")
+  i[2] = i[2].encode(Encoding.find("ASCII"), encoding_options)
+end
+
 # Start a new table
 newTable = [["Company Name", "Count of SC Clicks", "Count of Non-SC Clicks", "Total"]]
 
@@ -13,6 +29,9 @@ for i in table do
     newTable << [i[1], 0, i[3]]
 
   elsif
+    # check if there are matches for Company name.
+    # If so, amend that row in newTable.
+    # If not, make a new row in newTable.
     for x in newTable do
       if i[2].match(x[0]) && i[0].match('Count of Clicked ANY Smart Choice Rate')
         puts "match #{i[2]} and #{x[0]}"
@@ -29,8 +48,8 @@ end
 for t in newTable do
   t[3] = t[1].to_i+t[2].to_i
 end
-newTable[0][4] = "Total"
+newTable[0][3] = "Total"
 
 puts newTable
 puts "See newData.csv for the output"
-File.write("newData2.csv", newTable.map { |c| c.join(",") }.join("\n"))
+File.write("newData.csv", newTable.map { |c| c.join(",") }.join("\n"))
